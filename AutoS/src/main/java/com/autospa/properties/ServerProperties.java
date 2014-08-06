803a58ff3262bc767c542458e63c95ed443f106f
MateuszOlczak <Mateusz@Olczak>
2014-08-06 17:23:16 +0200

    Zmiany we frontendzie, upodobnienie struktury resources do MVC

--- AutoS/src/main/java/com/autospa/properties/ServerProperties.java
+++ AutoS/src/main/java/com/autospa/properties/ServerProperties.java
@@ -6,8 +6,8 @@ public class ServerProperties {
 
 	public static final String LOG_PATH_LINUX = "/home/dev/";
 	public static final String LOG_PATH_WINDOWS = "d:/";
-//	 public static final String SERVER_IP = "95.143.246.67";
-	public static final String SERVER_IP = "127.0.0.1";
+	 public static final String SERVER_IP = "95.143.246.67";
+//	public static final String SERVER_IP = "127.0.0.1";
 
 	public static final int TIME_PERIOD = 1000 * 90;
 	public static final int GET_ONE_PLN_STATE_TIME_PERIOD = 1000 * 120;
