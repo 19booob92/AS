982c57fc80dd19caa7d2eff173e8c49504064ac7
Mateusz <Mateusz@>
2014-08-18 15:01:16 +0200

    Zmiana jsp na thymeleaf

--- src/main/java/com/cohesiva/autospa/controller/LoginController.java
+++ src/main/java/com/cohesiva/autospa/controller/LoginController.java
@@ -2,25 +2,33 @@ package com.cohesiva.autospa.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.LockedException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.cohesiva.autospa.utils.ComponentModelAndView;
+
 @Controller
 public class LoginController {
+
+	@Autowired
+	private ComponentModelAndView model;
+	
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public ModelAndView login(@RequestParam(value = "error", required = false) String error,
 			@RequestParam(value = "logout", required = false) String logout, HttpServletRequest request) {
 
-		ModelAndView model = new ModelAndView();
 		if (error != null) {
 			model.addObject("error", getErrorMessage(request, "SPRING_SECURITY_LAST_EXCEPTION"));
 		}
@@ -38,7 +46,6 @@ public class LoginController {
 	public ModelAndView logOut(@RequestParam(value = "error", required = false) String error,
 			@RequestParam(value = "logout", required = false) String logout, HttpServletRequest request) {
 
-		ModelAndView model = new ModelAndView();
 		if (error != null) {
 			model.addObject("error", getErrorMessage(request, "SPRING_SECURITY_LAST_EXCEPTION"));
 		}
@@ -72,8 +79,6 @@ public class LoginController {
 	// for 403 access denied page
 	@RequestMapping(value = "/403", method = RequestMethod.GET)
 	public ModelAndView accesssDenied() {
-
-		ModelAndView model = new ModelAndView();
 
 		// check if user is login
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
---
+++ src/main/java/com/cohesiva/autospa/init/SecurityInit.java
@@ -0,0 +1,8 @@
+package com.cohesiva.autospa.init;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+
+}
+
